dd345b0f0db344bc2abedef9fd08dd141d5e60bf
msy20020410 <dj8817117@gmail.com>
2025-11-17 02:11:15 +0800

    Revert "fix: 新增用户"
    This reverts commit 113c544f44b01bda7e35e573e213a12f716042cb.

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -10,27 +10,10 @@ import java.io.Serializable;
 @ApiModel(description = "员工登录时传递的数据模型")
 public class EmployeeLoginDTO implements Serializable {
 
-    @ApiModelProperty("员工id")
-    private Integer id;
-
-    @ApiModelProperty("身份证")
-    private String idNumber;
-
-    @ApiModelProperty("姓名")
-    private String name;
-
-    @ApiModelProperty("手机号")
-    private String phone;
-
-    @ApiModelProperty("性别")
-    private String sex;
-
     @ApiModelProperty("用户名")
     private String username;
 
     @ApiModelProperty("密码")
     private String password;
-
-
 
 }
